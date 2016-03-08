9f986db3e48d5694b0a5aef81e81924a53a0fbb5
unknown <akash.123srm@gmail.com>
2016-03-08 16:43:32 +0100

    Added media and static folders

--- test_regio_app/urls.py
+++ test_regio_app/urls.py
@@ -1,2 +1,6 @@
 from django.conf.urls import patterns, url
-#from test_regio_app import views
+from test_regio_app import views
+
+urlpatterns = patterns('',
+    url(r'^landing_page', views.user_form_view, name='user_form_view'),
+    )
